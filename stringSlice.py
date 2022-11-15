78d65d61b82a052a56c23fbca75935d8bcdf45e9
danselo <danselo185@gmail.com>
2022-11-15 16:36:45 -0500

    add file stringSlice

---
+++ stringSlice.py
@@ -0,0 +1,18 @@
+#slicing = create a substring by extracting elements from another string
+# [start:stop:step]
+name = "Danilo Lora"
+
+#first_name = name[0:6]
+#print(first_name)
+#last_name = name[7:12]
+#funky_name = name[::7]
+#reversed_name = name[::-1]
+#print(funky_name)
+#print(last_name)
+#print(reversed_name)
+
+website = "http://google.com"
+website2 = "http://wikipedia.com"
+slice = slice(7,-4)
+print(website2[slice])
+print(website[slice])
