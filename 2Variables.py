39215cfb1aa8951673b4687f752256336a1fd227
danselo <danselo185@gmail.com>
2022-11-15 15:50:31 -0500

    add other themes for variables and methods

---
+++ 2Variables.py
@@ -0,0 +1,30 @@
+
+#Multiples assingment = allows us to assing multiple varianles a the same time in one line of code
+
+#name = "Danilo"
+#age = 20
+#attractive = True
+#print(name)
+#print(age)
+#print(attractive)
+#name, age, attractive = "Danilo", 20, True
+
+#Spongebob = 30
+#Patrick = 30
+#Sandy = 30
+#Squidward = 30
+
+#Spongebob = Patrick = Sandy = Squidward = 30
+
+name = "danilo"
+#print(len(name))
+#print(name.find("B"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("o"))
+#print(name.replace("o", "a"))
+#print(name*3)
+
