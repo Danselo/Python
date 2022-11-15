904d1cf8a84ccdf1b2b90788b5c1a29cc569e590
danselo <danselo185@gmail.com>
2022-11-15 15:57:45 -0500

    add file typeCasting

---
+++ typeCasting.py
@@ -0,0 +1,14 @@
+#type casting = convert the data type of a value to another data type
+
+x = 1 #int
+y = 2.0 #float
+z= "3" #str
+y = int(y)
+print(y)
+z = int(z)
+print(z)
+x= float(x)
+print(x)
+z= str(z)
+print(z)
+
