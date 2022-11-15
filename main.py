9482f6a683f91b7c87568a7520fe91a12d35ef2f
danselo <danselo185@gmail.com>
2022-11-15 15:38:20 -0500

    Variables python

---
+++ main.py
@@ -0,0 +1,28 @@
+﻿#print("I love pizza")
+#print("It's Really good!")
+# for contain comments in the code
+#variables
+#String
+#first_name = "Bro"
+#last_name = "Code"
+#full_name = first_name + " " + last_name
+#print(full_name)
+#print(type(name))
+#print("Hello " + full_name)
+
+#age = 21
+#age += 1
+#print(age)
+#print(type(age))
+#concatenate string with int
+#print("Your age is: "+ str(age))
+
+#height = 250.5
+#print(height)
+#print(type(height))
+#print("your height is: "+ str(height)+ "cm")
+
+human = False
+#print(human)
+#print(type(human))
+print("Are you a human : "+str(human))
